e0a97db4cd66f06235933cb00b5e8be2915b0644
enesbek <enes.bek.93@gmail.com>
2022-06-30 14:35:37 +0300

    initial commit

---
+++ src/components/Ebtn.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <button class="btn" :style="cssVars">
+      <slot/>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    variant: String,
+  },
+  computed: {
+    cssVars() {
+      if(this.variant == 'danger') {
+        return {'--color': 'red'}
+      }
+      else if( this.variant == 'success') {
+        return {
+          '--color': 'green', 
+          '--size': 1 + "em"}
+      }
+      else if(this.variant == 'primary') {
+        return {'--color': 'blue'}
+      }
+      else {
+        return {'--color': 'gray'}
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.btn{
+  cursor: pointer;
+  background-color: var(--color);
+  padding: var(--size);
+  @apply rounded font-semibold tracking-wide text-white;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Ebtn variant="success">I am a btn</Ebtn>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Ebtn from '@/components/Ebtn.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Ebtn
+  }
+}
+</script>
